7f4774e9f350e12c9cd5b05f556bc29dfd0d54e1
Sarathlal <hello@sarathlal.com>
2025-01-20 23:49:10 +0530

    Shotrtcode rendering compleeted

--- includes/class-cool-kids-network.php
+++ includes/class-cool-kids-network.php
@@ -167,6 +167,7 @@ class Cool_Kids_Network {
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		$this->loader->add_action( 'init', $plugin_public, 'register_short_code' );
 	}
 
 	/**
