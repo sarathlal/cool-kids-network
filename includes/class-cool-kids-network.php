249803d54a3630764d464afd13e7fbd29ef70741
Sarathlal <hello@sarathlal.com>
2025-01-21 10:48:50 +0530

    Custom user role and permission compleeted

--- includes/class-cool-kids-network.php
+++ includes/class-cool-kids-network.php
@@ -152,6 +152,7 @@ class Cool_Kids_Network {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		$this->loader->add_action( 'init', $plugin_admin, 'custom_user_roles' );
 	}
 
 	/**
