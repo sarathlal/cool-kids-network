7852558f099adf19d8a0053c8c96d9cce615fcb5
Sarathlal <hello@sarathlal.com>
2025-01-21 09:16:45 +0530

    Login functionality compleeted

--- includes/class-cool-kids-network.php
+++ includes/class-cool-kids-network.php
@@ -168,6 +168,7 @@ class Cool_Kids_Network {
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
 		$this->loader->add_action( 'init', $plugin_public, 'register_short_code' );
+		$this->loader->add_action( 'admin_post_nopriv_ckn_login', $plugin_public, 'login' );
 	}
 
 	/**
